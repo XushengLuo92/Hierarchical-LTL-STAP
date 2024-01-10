e190e80c33b2d3c99fa597073b3abfc2f77c4fa7
Xusheng Luo <xusheng.luo2@gmail.com>
2024-01-10 13:34:46 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,30 @@
-for i in {14..14}
+for i in {12..14}
 do
     echo "------------- $i --------------" 
     echo "------------- heuristics_all  --------------" 
     for j in {1..20}
     do
-        time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics --heuristic_weight=100 --domain_file=/Users/xushengluo/Documents/Code/Hierarchical-LTL-STAP/hierarchical_ltl_stap/domain_bosch.json  --domain=bosch --num_robots=2 
+        time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics --heuristic_weight=100 --domain_file=./domain/domain_bosch.json  --domain=bosch --num_robots=2 
     done
     # echo "------------- heuristics_order  --------------" 
     # for j in {1..10}
     # do
-    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics_order --heuristic_weight=100 --domain_file=/Users/xushengluo/Documents/Code/Hierarchical-LTL-STAP/hierarchical_ltl_stap/domain_bosch.json  --domain=bosch --num_robots=2 
+    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics_order --heuristic_weight=100 --domain_file=./domain/domain_bosch.json  --domain=bosch --num_robots=2 
     # done
     # echo "------------- heuristics_switch --------------" 
     # for j in {1..10}
     # do
-    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics_switch --heuristic_weight=100 --domain_file=/Users/xushengluo/Documents/Code/Hierarchical-LTL-STAP/hierarchical_ltl_stap/domain_bosch.json  --domain=bosch --num_robots=2
+    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics_switch --heuristic_weight=100 --domain_file=./domain/domain_bosch.json  --domain=bosch --num_robots=2
     # done
     # echo "------------- heuristics_automaton --------------" 
     # for j in {1..10}
     # do
-    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics_automaton --heuristic_weight=100 --domain_file=/Users/xushengluo/Documents/Code/Hierarchical-LTL-STAP/hierarchical_ltl_stap/domain_bosch.json  --domain=bosch --num_robots=2
+    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics_automaton --heuristic_weight=100 --domain_file=./domain/domain_bosch.json  --domain=bosch --num_robots=2
     # done
     # echo "------------- heuristics_no  --------------" 
     # for j in {1..10}
     # do
-    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristic_weight=100 --domain_file=/Users/xushengluo/Documents/Code/Hierarchical-LTL-STAP/hierarchical_ltl_stap/domain_bosch.json  --domain=bosch --num_robots=2 
+    #     time gtimeout 1h  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristic_weight=100 --domain_file=./domain/domain_bosch.json  --domain=bosch --num_robots=2 
     # done
    
 done
@@ -35,7 +35,7 @@ done
 #     do
 #         for j in {1..20}
 #         do 
-#             time gtimeout 30m  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics --heuristic_weight=100 --domain_file=/Users/xushengluo/Documents/Code/Hierarchical-LTL-STAP/src/domain_bosch.json  --domain=bosch --num_robots=$n
+#             time gtimeout 30m  python hierarchical_LTL_stap_on_the_fly.py --task=nav --case=$i --heuristics --heuristic_weight=100 --domain_file=./domain/domain_bosch.json  --domain=bosch --num_robots=$n
 #         done
 #     done
 # done
